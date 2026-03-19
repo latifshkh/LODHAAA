9a8c5c1c954779d4c1140bba51ae208dc6055d36
latifshkh <slatif200426@gmail.com>
2026-03-19 09:01:51 +0530

    bulk csv img

--- lib/appwrite.ts
+++ lib/appwrite.ts
@@ -1,12 +1,22 @@
 import { Client, Databases } from "appwrite";
 
+const endpoint = process.env.NEXT_PUBLIC_APPWRITE_ENDPOINT;
+const project = process.env.NEXT_PUBLIC_APPWRITE_PROJECT;
+
+if (!endpoint || !project) {
+  throw new Error("Missing Appwrite ENV variables");
+}
+
 const client = new Client();
 
 client
-  .setEndpoint(process.env.NEXT_PUBLIC_APPWRITE_ENDPOINT!)
-  .setProject(process.env.NEXT_PUBLIC_APPWRITE_PROJECT!);
+  .setEndpoint(endpoint)
+  .setProject(project);
 
 export const databases = new Databases(client);
 
-export const DATABASE_ID = process.env.NEXT_PUBLIC_APPWRITE_DATABASE_ID!;
-export const RESIDENCES_COLLECTION_ID = process.env.NEXT_PUBLIC_APPWRITE_COLLECTION_ID!;
+export const DATABASE_ID =
+  process.env.NEXT_PUBLIC_APPWRITE_DATABASE_ID!;
+
+export const RESIDENCES_COLLECTION_ID =
+  process.env.NEXT_PUBLIC_APPWRITE_COLLECTION_ID!;
